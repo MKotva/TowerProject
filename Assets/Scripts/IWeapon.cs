cdb7157e551bc5cabeaf63671d2a2e88a94042ef
Maty83 <matyashalir@gmail.com>
2025-11-29 21:59:01 +0100

    Finish string hints for doors.

--- Assets/Scripts/IWeapon.cs
+++ Assets/Scripts/IWeapon.cs
@@ -11,3 +11,12 @@ public interface IWeapon
     public int Range { get; set; }
     public List<Vector2Int> AffectedTiles { get; set; } //Tiles affected around the targeted tile. Would be replaced by floating point range in case of 2D combat
 }
+
+public class Sword : IWeapon
+{
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public PlayerStats BonusStats { get; set; }
+    public int Range { get; set; }
+    public List<Vector2Int> AffectedTiles { get; set; }
+}
