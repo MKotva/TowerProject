b9a5103a50736b4ff6e2e5f8f597611eda74ad58
Maty83 <matyashalir@gmail.com>
2025-11-29 23:22:44 +0100

    Create rest of weapons with minimal necessary statistics.

--- Assets/Scripts/IWeapon.cs
+++ Assets/Scripts/IWeapon.cs
@@ -9,6 +9,9 @@ public interface IWeapon
     public string Description { get; set; }
     public PlayerStats BonusStats { get; set; }
     public int Range { get; set; }
+    public int MinDMG {  get; set; }
+    public float MaxDMG { get; set; }
+
     public int BlockDamageReduction { get; set; }
     public List<Vector2Int> GetAffectedTiles(Vector2Int Pos, Vector2Int Tgt); //Tiles affected around the targeted tile. Would be replaced by floating point range in case of 2D combat
 }
@@ -17,12 +20,27 @@ public class Sword : IWeapon
 {
     public string Name { get; set; } = "";
     public string Description { get; set; } = "";
-    public PlayerStats BonusStats { get; set; }=new PlayerStats();
+    public PlayerStats BonusStats { get; set; } = new PlayerStats();
     public int Range { get; set; } = 1;
+    public int MinDMG { get; set; }
+    public float MaxDMG { get; set; }
     public int BlockDamageReduction { get; set; } = 5;
     public List<Vector2Int> GetAffectedTiles(Vector2Int Pos, Vector2Int Tgt)
     {
-        throw new NotImplementedException();
+        var l = new List<Vector2Int>() { new Vector2Int(0, 0) };
+        var diffVec = Tgt - Pos;
+
+        if (diffVec.x * diffVec.x >= diffVec.y * diffVec.y)
+        {
+            l.Add(new Vector2Int(1, 0));
+            l.Add(new Vector2Int(-1, 0));
+        }
+        else
+        {
+            l.Add(new Vector2Int(0, 1));
+            l.Add(new Vector2Int(0, -1));
+        }
+        return l;
     }
 }
 
@@ -32,10 +50,36 @@ public class Spear: IWeapon
     public string Description { get; set; } = "";
     public PlayerStats BonusStats { get; set; } = new PlayerStats();
     public int Range { get; set; } = 1;
+    public int MinDMG { get; set; }
+    public float MaxDMG { get; set; }
     public int BlockDamageReduction { get; set; } = 1;
     public List<Vector2Int> GetAffectedTiles(Vector2Int Pos, Vector2Int Tgt)
     {
-        throw new NotImplementedException();
+        var l=new List<Vector2Int>() { new Vector2Int(0, 0) };
+        var diffVec = Tgt - Pos;
+        if (diffVec.x * diffVec.x >= diffVec.y * diffVec.y)
+        {
+            if (diffVec.x > 0)
+            {
+                l.Add(new Vector2Int(1, 0));
+            }
+            else
+            {
+                l.Add(new Vector2Int(-1, 0));
+            }
+        }
+        else
+        {
+            if (diffVec.y > 0) 
+            {
+                l.Add(new Vector2Int(0, 1));
+            }
+            else
+            {
+                l.Add(new Vector2Int(0, -1));
+            }
+        }
+        return l;
     }
 }
 public class Bow: IWeapon
@@ -44,10 +88,12 @@ public class Bow: IWeapon
     public string Description { get; set; } = "";
     public PlayerStats BonusStats { get; set; } = new PlayerStats();
     public int Range { get; set; } = 8;
+    public int MinDMG { get; set; }
+    public float MaxDMG { get; set; }
     public int BlockDamageReduction { get; set; } = 1;
     public List<Vector2Int> GetAffectedTiles(Vector2Int Pos, Vector2Int Tgt)
     {
-        throw new NotImplementedException();
+        return new List<Vector2Int>() { new Vector2Int(0, 0) };
     }
 }
 public class Shield: IWeapon
@@ -56,10 +102,12 @@ public class Shield: IWeapon
     public string Description { get; set; } = "";
     public PlayerStats BonusStats { get; set; } = new PlayerStats();
     public int Range { get; set; } = 1;
+    public int MinDMG { get; set; }
+    public float MaxDMG { get; set; }
     public int BlockDamageReduction { get; set; } = 1;
     public List<Vector2Int> GetAffectedTiles(Vector2Int Pos, Vector2Int Tgt)
     {
-        throw new NotImplementedException();
+        return new List<Vector2Int>() { new Vector2Int(0, 0) };
     }
 }
 public class MagicStaff: IWeapon
@@ -68,9 +116,11 @@ public class MagicStaff: IWeapon
     public string Description { get; set; } = "";
     public PlayerStats BonusStats { get; set; } = new PlayerStats();
     public int Range { get; set; } = 1;
+    public int MinDMG { get; set; }
+    public float MaxDMG { get; set; }
     public int BlockDamageReduction { get; set; } = 1;
     public List<Vector2Int> GetAffectedTiles(Vector2Int Pos, Vector2Int Tgt)
     {
-        throw new NotImplementedException();
+        return new List<Vector2Int>() { new Vector2Int(-1, 0), new Vector2Int(1, 0), new Vector2Int(0, 0), new Vector2Int(0, 1), new Vector2Int(0, -1) };
     }
 }
