bf554cb5ba413785b2f70739990b94eadc8a283e
Maty83 <matyashalir@gmail.com>
2025-11-29 22:19:44 +0100

    Start on weapon generation

--- Assets/Scripts/IWeapon.cs
+++ Assets/Scripts/IWeapon.cs
@@ -9,14 +9,68 @@ public interface IWeapon
     public string Description { get; set; }
     public PlayerStats BonusStats { get; set; }
     public int Range { get; set; }
-    public List<Vector2Int> AffectedTiles { get; set; } //Tiles affected around the targeted tile. Would be replaced by floating point range in case of 2D combat
+    public int BlockDamageReduction { get; set; }
+    public List<Vector2Int> GetAffectedTiles(Vector2Int Pos, Vector2Int Tgt); //Tiles affected around the targeted tile. Would be replaced by floating point range in case of 2D combat
 }
 
 public class Sword : IWeapon
 {
-    public string Name { get; set; }
-    public string Description { get; set; }
-    public PlayerStats BonusStats { get; set; }
-    public int Range { get; set; }
-    public List<Vector2Int> AffectedTiles { get; set; }
+    public string Name { get; set; } = "";
+    public string Description { get; set; } = "";
+    public PlayerStats BonusStats { get; set; }=new PlayerStats();
+    public int Range { get; set; } = 1;
+    public int BlockDamageReduction { get; set; } = 5;
+    public List<Vector2Int> GetAffectedTiles(Vector2Int Pos, Vector2Int Tgt)
+    {
+        throw new NotImplementedException();
+    }
+}
+
+public class Spear: IWeapon
+{
+    public string Name { get; set; } = "";
+    public string Description { get; set; } = "";
+    public PlayerStats BonusStats { get; set; } = new PlayerStats();
+    public int Range { get; set; } = 1;
+    public int BlockDamageReduction { get; set; } = 1;
+    public List<Vector2Int> GetAffectedTiles(Vector2Int Pos, Vector2Int Tgt)
+    {
+        throw new NotImplementedException();
+    }
+}
+public class Bow: IWeapon
+{
+    public string Name { get; set; } = "";
+    public string Description { get; set; } = "";
+    public PlayerStats BonusStats { get; set; } = new PlayerStats();
+    public int Range { get; set; } = 8;
+    public int BlockDamageReduction { get; set; } = 1;
+    public List<Vector2Int> GetAffectedTiles(Vector2Int Pos, Vector2Int Tgt)
+    {
+        throw new NotImplementedException();
+    }
+}
+public class Shield: IWeapon
+{
+    public string Name { get; set; } = "";
+    public string Description { get; set; } = "";
+    public PlayerStats BonusStats { get; set; } = new PlayerStats();
+    public int Range { get; set; } = 1;
+    public int BlockDamageReduction { get; set; } = 1;
+    public List<Vector2Int> GetAffectedTiles(Vector2Int Pos, Vector2Int Tgt)
+    {
+        throw new NotImplementedException();
+    }
+}
+public class MagicStaff: IWeapon
+{
+    public string Name { get; set; } = "";
+    public string Description { get; set; } = "";
+    public PlayerStats BonusStats { get; set; } = new PlayerStats();
+    public int Range { get; set; } = 1;
+    public int BlockDamageReduction { get; set; } = 1;
+    public List<Vector2Int> GetAffectedTiles(Vector2Int Pos, Vector2Int Tgt)
+    {
+        throw new NotImplementedException();
+    }
 }
