33bac5bc936eff80ed8d3691cc85be0720c081eb
Maty83 <matyashalir@gmail.com>
2025-11-29 12:06:02 +0100

    Fix IWeapon list

--- Assets/Scripts/IWeapon.cs
+++ Assets/Scripts/IWeapon.cs
@@ -1,13 +1,13 @@
+using System.Collections.Generic;
 using NUnit.Framework;
 using System;
 using UnityEngine;
 
-public class IWeapon
+public interface IWeapon
 {
     public string Name { get; set; }
     public string Description { get; set; }
     public PlayerStats BonusStats { get; set; }
     public int Range { get; set; }
-    //public List<Vector2Int> AffectedTiles { get; set; } //I am not sure why TF this doesn't work
-
+    public List<Vector2Int> AffectedTiles { get; set; } //Tiles affected around the targeted tile. Would be replaced by floating point range in case of 2D combat
 }
