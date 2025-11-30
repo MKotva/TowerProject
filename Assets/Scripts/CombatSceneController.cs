467113d1a7dfbda5cfa0ab6612ae929293da6b67
Maty83 <matyashalir@gmail.com>
2025-11-30 15:09:46 +0100

    Start work on enemies actually attacking

--- Assets/Scripts/CombatSceneController.cs
+++ Assets/Scripts/CombatSceneController.cs
@@ -32,8 +32,9 @@ public class CombatSceneController : MonoBehaviour
 
     public SelectedAction curSelectedAction = SelectedAction.None;
     private int TurnState=-1;
-    private bool TurnDisabled=false;
-    
+    private bool TimerDisableMove=false;
+    private float totDelta = 0;
+    private bool enemyMoved = false;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -75,31 +76,66 @@ public class CombatSceneController : MonoBehaviour
     //Method for grid buttons to send signal that they've been pressed.
     public void ReceiveClick(int w, int h)
     {
-        //TODO: Logic of what happens
-        Debug.Log(w + ", " + h);
-        switch (curSelectedAction)
+        if (!TimerDisableMove && TurnState == -1)
         {
-            case SelectedAction.Move:
-                MoveGO(w, h, player.gameObject);
-                playerPos = new Vector2Int(w, h);
-                TurnState++;
-                break;
-            case SelectedAction.Attack:
-                break;
-            default:
-                break;
+            //TODO: Logic of what happens
+            Debug.Log(w + ", " + h);
+            switch (curSelectedAction)
+            {
+                case SelectedAction.Move:
+                    if (IsOccupied(new Vector2Int(w, h)))
+                    {
+                        return;
+                    }
+                    MoveGO(w, h, player.gameObject);
+                    playerPos = new Vector2Int(w, h);
+                    curSelectedAction = SelectedAction.None;
+                    break;
+                case SelectedAction.Attack:
+                    if (PlayerAttack(w, h))
+                    {
+                        TurnState++;
+                    }
+                    else
+                    {
+                        curSelectedAction = SelectedAction.None;
+                        return;
+                    }
+                    break;
+                default:
+                    break;
+            }
+            curSelectedAction = SelectedAction.None;
+
         }
-        curSelectedAction = SelectedAction.None;       
+    }
+    public bool PlayerAttack(int w, int h)
+    {
+        if (Math.Abs(playerPos.x - w) + Math.Abs(playerPos.y - h) > player.Weapon.Range)
+        {
+            return false;
+        }
+        for (int i = 0; i<enemiesPos.Count; i++)
+        {
+            if (enemiesPos[i].Item2.x==w && enemiesPos[i].Item2.y == h)
+            {
+                //If there is an enemy, we attack
+                enemiesPos[i].Item1.RecieveDamage(player.Weapon.DamagePower);
+                return true;
+            }            
+        }
+        return false;
     }
     public void MoveGO(int w, int h, GameObject go)
     {
         //go.transform.position=CellArray[w, h].transform.position;
         go.transform.DOMove(CellArray[w, h].transform.position, 1);
+        TimerDisableMove = true;
     }
     // Update is called once per frame
     void Update()
     {
-        if (!TurnDisabled)
+        if (!TimerDisableMove)
         {
             if (TurnState == -1)
             {
@@ -107,12 +143,22 @@ public class CombatSceneController : MonoBehaviour
             }
             else
             {
-                bool endedTurn = getEnemyAction(TurnState);
+                bool endedTurn = GetEnemyAction(TurnState);
                 //TODO: This should move the enemy,. in future also lock controls
                 if (endedTurn)
                 {
+                    enemyMoved = false;
                     TurnState++;
                 }
+            }
+        }
+        else
+        {
+            totDelta += Time.deltaTime;
+            if (totDelta > 1)
+            {
+                totDelta = 0;
+                TimerDisableMove = false;
             }
         }
         if (TurnState >= enemies.Count)
@@ -120,13 +166,94 @@ public class CombatSceneController : MonoBehaviour
             TurnState = -1;
         }
     }
-    private bool getEnemyAction(int idx)
+    private bool GetEnemyAction(int idx)
     {
-        Entity enemy= enemiesPos[idx].Item1;
-        Vector2Int curEnemyPos= enemiesPos[idx].Item2;
-        Vector2Int resultPos=curEnemyPos;
-        const int max_move= 3;
-        if (Math.Abs(curEnemyPos.x - playerPos.x) + Math.Abs(curEnemyPos.y - playerPos.y) > 1)
+        //Move block: If we are too far, move up.
+        Entity enemy = enemiesPos[idx].Item1;
+        var enemyPos=enemiesPos[idx].Item2;
+        var weapRange = enemy.Weapon.Range;
+        if (Math.Abs(enemyPos.x - playerPos.x) + Math.Abs(enemyPos.y - playerPos.y) > weapRange && !enemyMoved)
+        {
+            var resultPos = ReturnDesiredPos(idx);
+            /*if (resultPos != enemiesPos[idx].Item2)*/
+            {
+                resultPos = VerifyKickout(resultPos);
+                resultPos = new Vector2Int(Math.Clamp(resultPos.x, 0, GridWidth - 1), Math.Clamp(resultPos.y, 0, GridHeight - 1));
+                MoveGO(resultPos.x, resultPos.y, enemy.gameObject);
+            }
+            enemiesPos[idx] = new Tuple<Entity, Vector2Int>(enemy, resultPos);
+            enemyMoved = true;
+        }
+        
+        return true;
+    }
+    public Vector2Int VerifyKickout(Vector2Int desired)
+    {
+        //We can try +-1 positions only.
+        if (!IsOccupied(desired))
+        {
+            return desired;
+        }
+        if (!IsOccupied(desired + new Vector2Int(1, 0)))
+        {
+            return desired + new Vector2Int(1, 0);
+        }
+        if (!IsOccupied(desired + new Vector2Int(-1, 0)))
+        {
+            return desired + new Vector2Int(-1, 0);
+        }
+        if (!IsOccupied(desired + new Vector2Int(0, 1)))
+        {
+            return desired + new Vector2Int(0, 1);
+        }
+        if (!IsOccupied(desired + new Vector2Int(0, -1)))
+        {
+            return desired + new Vector2Int(0, -1);
+        }
+        if (!IsOccupied(desired + new Vector2Int(1, 1)))
+        {
+            return desired + new Vector2Int(1, 1);
+        }
+        if (!IsOccupied(desired + new Vector2Int(-1, 1)))
+        {
+            return desired + new Vector2Int(-1, 1);
+        }
+        if (!IsOccupied(desired + new Vector2Int(1, -1)))
+        {
+            return desired + new Vector2Int(1, -1);
+        }
+        if (!IsOccupied(desired + new Vector2Int(-1, -1)))
+        {
+            return desired + new Vector2Int(-1, -1);
+        }
+        return desired;
+    }
+    public bool IsOccupied(Vector2Int pos)
+    {
+        bool occupied=false;
+        if (playerPos.x == pos.x && playerPos.y == pos.y)
+        {
+            occupied = true;
+        }
+        for (int i = 0; i<enemiesPos.Count; i++)
+        {
+            if (enemiesPos[i].Item2.x == pos.x && enemiesPos[i].Item2.y== pos.y)
+            {
+                occupied = true;
+            }
+        }
+        if (pos.x< 0|| pos.y < 0 || pos.x >= GridWidth || pos.y >= GridHeight)
+        {
+            occupied=true;
+        }
+        return occupied;
+    }
+    public Vector2Int ReturnDesiredPos(int idx, int max_move=3)
+    {
+        Entity enemy = enemiesPos[idx].Item1;
+        Vector2Int curEnemyPos = enemiesPos[idx].Item2;
+        Vector2Int resultPos = curEnemyPos;
+        if (Math.Abs(curEnemyPos.x - playerPos.x) + Math.Abs(curEnemyPos.y - playerPos.y) > 0)
         {
             //move
             if (Math.Abs(curEnemyPos.x - playerPos.x) > 3)
@@ -156,22 +283,19 @@ public class CombatSceneController : MonoBehaviour
             {
                 var xMove = curEnemyPos.x - playerPos.x;
                 var xAbs = Mathf.Abs(xMove);
-                var yMove = Mathf.Min(max_move - xAbs, Mathf.Abs(curEnemyPos.y-playerPos.y));
+                var yMove = Mathf.Min(max_move - xAbs, Mathf.Abs(curEnemyPos.y - playerPos.y));
                 if (curEnemyPos.y < playerPos.y)
                 {
                     yMove *= -1;
                 }
-                resultPos = new Vector2Int(curEnemyPos.x - (curEnemyPos.x - playerPos.x), curEnemyPos.y-yMove);                
+                resultPos = new Vector2Int(curEnemyPos.x - (curEnemyPos.x - playerPos.x), curEnemyPos.y - yMove);
             }
-            resultPos = new Vector2Int(Math.Clamp(resultPos.x, 0,GridWidth-1), Math.Clamp(resultPos.y,0,GridHeight-1));
-            MoveGO(resultPos.x, resultPos.y, enemy.gameObject);//Bug
-            enemiesPos[idx] = new Tuple<Entity, Vector2Int>(enemy,resultPos);
+            resultPos = new Vector2Int(Math.Clamp(resultPos.x, 0, GridWidth - 1), Math.Clamp(resultPos.y, 0, GridHeight - 1));
         }
-        //We should select and execute enemy action here
-        return true;
+        return resultPos;
     }
     public void OnTimeout()
     {
-        TurnDisabled = false;
+        TimerDisableMove = false;
     }
 }
